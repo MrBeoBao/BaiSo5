4033df93b5f4d70dc7382c7c91c596ae48a16b75
Anh Tuấn <anhtuan07032006@gmail.com>
2026-04-21 23:14:30 +0700

    Update DoAn.java

--- DoAn/src/doan/DoAn.java
+++ DoAn/src/doan/DoAn.java
@@ -1,8 +1,35 @@
-
 package doan;
 
-public class DoAn {
-    public static void main(String[] args) {
+import javax.swing.;
+import java.awt.;
+import java.awt.event.;
+
+public class DoAn extends JFrame implements ActionListener {
+
+    public String giaiPT(double a, double b, double c) {
+        if (a == 0) {
+            if (b == 0) {
+                return (c == 0) ? "Vô số nghiệm" : "Vô nghiệm";
+            } else {
+                return "x = " + (-c / b);
+            }
+        }
+
+        double delta = b b - 4 * a * c;
+
+        if (delta < 0) {
+            return "Vô nghiệm";
+        } else if (delta == 0) {
+            double x = -b / (2 * a);
+            return "Nghiệm kép: x = " + x;
+        } else {
+            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return "x1 = " + x1 + "\nx2 = " + x2;
+        }
     }
-    
+
+    public static void main(String[] args) {
+        new DoAn().setVisible(true);
+    }
 }

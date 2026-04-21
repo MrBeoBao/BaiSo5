cca103dba513d46b0282d83f1802a92ab7404f15
Diệu Hiền <dhhiendieu0406@gmail.com>
2026-04-21 23:14:34 +0700

    Update DoAn.java

--- DoAn/src/doan/DoAn.java
+++ DoAn/src/doan/DoAn.java
@@ -1,8 +1,58 @@
-
 package doan;
 
-public class DoAn {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class DoAn extends JFrame implements ActionListener {
+
+    private JTextField txtA, txtB, txtC;
+    private JButton btnGiai;
+    private JTextArea txtKetQua;
+
+    public DoAn() {
+        setTitle("Giải phương trình bậc 2");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panelInput = new JPanel(new GridLayout(3, 2));
+
+        panelInput.add(new JLabel("a:"));
+        txtA = new JTextField();
+        panelInput.add(txtA);
+
+        panelInput.add(new JLabel("b:"));
+        txtB = new JTextField();
+        panelInput.add(txtB);
+
+        panelInput.add(new JLabel("c:"));
+        txtC = new JTextField();
+        panelInput.add(txtC);
+
+        btnGiai = new JButton("Giải");
+        btnGiai.addActionListener(this);
+
+        txtKetQua = new JTextArea();
+        txtKetQua.setEditable(false);
+
+        add(panelInput, BorderLayout.NORTH);
+        add(btnGiai, BorderLayout.CENTER);
+        add(txtKetQua, BorderLayout.SOUTH);
     }
-    
+
+    public void actionPerformed(ActionEvent e) {
+        try {
+            double a = Double.parseDouble(txtA.getText());
+            double b = Double.parseDouble(txtB.getText());
+            double c = Double.parseDouble(txtC.getText());
+
+            txtKetQua.setText(giaiPT(a, b, c));
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Nhập sai!");
+        }
+    }
+
+
 }
